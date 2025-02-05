c29e948b5473818f8a6ee8918deba9545f556d52
Utkarsh <utkarshcpark@gmail.com>
2025-02-06 02:27:34 +0530

    Remove api key

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,2 @@
 //Ideally this should be in an .env file,but I ahve added in constants for the sake of simplicity.
-const String GOOGLE_MAP_API_KEY = "Add your key here";
+const String GOOGLE_MAP_API_KEY = "Add your api key";
